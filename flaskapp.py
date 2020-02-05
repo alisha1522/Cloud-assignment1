5a044071c57766283f611a5db6360d4fd28ca5a3
Ubuntu <ubuntu@ip-172-31-12-123.us-east-2.compute.internal>
2020-02-05 01:36:00 +0000

    Added functionality for download. #closes 1

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,6 @@
 
 import sqlite3
-from flask import Flask, render_template, request, redirect ,g, url_for
+from flask import Flask, render_template, request, redirect ,g, url_for, send_file
 import os
 app = Flask(__name__)
 DATABASE='/home/ubuntu/flaskapp/db/users.db'
@@ -28,9 +28,9 @@ def login():
                     return render_template('Registration_page.html', us_pwd_exist='false')
                 else:
 		    if row[5]:
-			return render_template('display.html',firstname=row[0],lastname=row[1],email=row[2],username=row[3],filename=row[5],count=row[6])
+			return render_template('display.html',firstname=row[0],lastname=row[1],email=row[2],username=row[3],password=row[4],filename=row[5],count=row[6])
 		    else:
-                    	return render_template('display.html',firstname=row[0],lastname=row[1],email=row[4])
+                    	return render_template('display.html',firstname=row[0],lastname=row[1],email=row[2],username=row[3],password=row[4])
             db.close()
 
     return render_template('Registration_page.html')
@@ -48,13 +48,13 @@ def register():
         if db is None:
             db = g._database = sqlite3.connect(DATABASE)
             cur = db.cursor()
-            cur.execute('select * from user where user_Name =?', (username))
+            cur.execute('select * from user where user_Name =?', (username,))
             row = cur.fetchone()
             if(row == None ):
-            	cur.execute('select * from user where email =?', (email))
+           	cur.execute('select * from user where email =?', (email,))
                 row = cur.fetchone()
                 if(row == None):
-                   cur.execute("insert into user(first_Name, last_Name, user_Name,password,email) values (?,?,?,?,?)",(first_Name,last_Name,username,password,email))
+                   cur.execute("insert into user(first_Name, last_Name, user_Name,password,email,file,count) values (?,?,?,?,?,NULL,NULL)",(first_Name,last_Name,username,password,email,))
                    db.commit()
                 else:
                    return render_template('Second.html', em_exist='true')
@@ -72,6 +72,7 @@ def upload():
 	if request.method == 'POST':
        	 f = request.files['file']
 	 username=request.form['username']
+	 password=request.form['password']
        	 filename = f.filename
        	 f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 	 word_count = count(os.path.join(app.config['UPLOAD_FOLDER'], filename))
@@ -81,7 +82,24 @@ def upload():
 		cur = db.cursor()
 		cur.execute('update user set file=?, count=? where user_Name =?', (filename,word_count,username))
 		db.commit()
-		return redirect( url_for('login'))
+            	cur = db.cursor()
+            	cur.execute('select * from user where user_Name =?', (username,))
+            	row = cur.fetchone()
+            	if(row == None):
+                	return render_template('Registration_page.html', us_exist='false')
+            	else:
+                	cur.execute('select * from user where user_Name =? and password =?', (username, password,))
+                	row = cur.fetchone()
+                	if (row == None):
+                    		return render_template('Registration_page.html', us_pwd_exist='false')
+                	else:
+                    		if row[5]:
+            				db.close()
+                        		return render_template('display.html',firstname=row[0],lastname=row[1],email=row[2],username=row[3],filename=row[5],count=row[6])
+                    		else:
+            				db.close()
+                        		return render_template('display.html',firstname=row[0],lastname=row[1],email=row[4])
+
 	return render_template('display.html')
 
 def count(file):
@@ -92,5 +110,8 @@ def count(file):
                 word += len(words)
         return word
 
+@app.route('/download/<path:filename>' , methods=['GET', 'POST'])
+def download(filename):
+	return send_file(os.path.join(app.config['UPLOAD_FOLDER'], filename), attachment_filename=filename, as_attachment=True)
 if __name__ == '__main__':
 	app.run()
